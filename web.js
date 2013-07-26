1ef9e43d70d32057838ed9d0424e01f641c52329
V Neethan <example@stanford.edu>
2013-07-26 05:17:52 +0000

    Adding medmana files

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-var outfile = "index.html";
+var outfile = "medindex.html";
 var readfile =fs.readFileSync(outfile);
 var btos = readfile.toString();
 
