d0088e88e3f10b5726ca0c5d729ce9b413179fb8
V Neethan <example@stanford.edu>
2013-07-26 05:02:14 +0000

    Trying to put medmana on heroku

---
+++ web.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var fs = require('fs');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+var outfile = "index.html";
+var readfile =fs.readFileSync(outfile);
+var btos = readfile.toString();
+
+response.send(btos) ;
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
